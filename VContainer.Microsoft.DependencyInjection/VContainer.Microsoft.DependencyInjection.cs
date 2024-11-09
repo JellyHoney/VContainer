187338411ca287b3ebec8913917b2b5b2ed9960d
Jelly Honey <494883350@qq.com>
2024-11-09 12:13:06 +0800

    支持微软DI

---
+++ VContainer.Microsoft.DependencyInjection/VContainer.Microsoft.DependencyInjection.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection.Metadata.Ecma335;
+using VContainer.Internal;
+
+namespace VContainer.Microsoft.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceProvider CreateVContainerServiceProvider(this IServiceCollection serviceCollection)
+        {
+            var container = new ContainerBuilder();
+            return container.BuildServiceProvider(serviceCollection);
+        }
+    }
+
+    public static class ContainerBuilderExtensions
+    {
+        public static IServiceProvider BuildServiceProvider(this ContainerBuilder builder, IServiceCollection serviceCollection)
+        {
+            builder.Register<IServiceProvider>(resolver => new VContainerServiceProvider(resolver), Lifetime.Scoped);
+            builder.Register<IServiceScopeFactory>(resolver => new VContainerScopedFactory(resolver), Lifetime.Singleton);
+
+            foreach (var service in serviceCollection)
+            {
+                builder.AddServices(service);
+            }
+
+            var resolver = builder.Build();
+            return new VContainerServiceProvider(resolver);
+        }
+
+        private static Lifetime Convert2VContainerLifetime(ServiceLifetime lifetime)
+        {
+            return lifetime switch
+            {
+                ServiceLifetime.Singleton => Lifetime.Singleton,
+                ServiceLifetime.Scoped => Lifetime.Scoped,
+                ServiceLifetime.Transient => Lifetime.Transient,
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        private static void AddServices(this ContainerBuilder builder, ServiceDescriptor serviceDescriptor)
+        {
+            var lifetime = Convert2VContainerLifetime(serviceDescriptor.Lifetime);
+            if (serviceDescriptor.ImplementationInstance != null)
+            {
+                var registraction = new InstanceRegistrationBuilder(serviceDescriptor.ImplementationInstance);
+                builder.Register(registraction);
+            }
+            else if (serviceDescriptor.ImplementationFactory != null)
+            {
+                var registration = new FuncRegistrationBuilder(resolver =>
+                {
+                    var provider = resolver.Resolve<IServiceProvider>();
+                    return serviceDescriptor.ImplementationFactory(provider);
+                }, serviceDescriptor.ImplementationType, lifetime);
+                builder.Register(registration);
+            }
+            else
+            {
+                builder.Register(serviceDescriptor.ServiceType, serviceDescriptor.ImplementationType, lifetime);
+            }
+        }
+    }
+
+    public class VContainerServiceProvider : IServiceProvider, ISupportRequiredService, IDisposable
+    {
+        private readonly IObjectResolver mObjectResolver;
+
+        public VContainerServiceProvider(IObjectResolver objectResolver)
+        {
+            mObjectResolver = objectResolver;
+        }
+
+        public void Dispose()
+        {
+            mObjectResolver.Dispose();
+        }
+
+        public object GetRequiredService(Type serviceType)
+        {
+            return mObjectResolver.Resolve(serviceType);
+        }
+
+        public object? GetService(Type serviceType)
+        {
+            mObjectResolver.TryResolve(serviceType, out var ret);
+            return ret;
+        }
+    }
+
+    public class VContainerScopedFactory(IObjectResolver objectResolver) : IServiceScopeFactory
+    {
+        private readonly IObjectResolver mObjectResolver = objectResolver;
+
+        public IServiceScope CreateScope()
+        {
+            return new VContainerScope(mObjectResolver.CreateScope());
+        }
+    }
+
+    public class VContainerScope : IServiceScope
+    {
+        private IObjectResolver mObjectResolver;
+        private VContainerServiceProvider mServiceProvider;
+
+        public VContainerScope(IObjectResolver mObjectResolver)
+        {
+            this.mObjectResolver = mObjectResolver;
+            mServiceProvider = new VContainerServiceProvider(mObjectResolver);
+        }
+
+        public IServiceProvider ServiceProvider => mServiceProvider;
+
+        public void Dispose()
+        {
+            mServiceProvider.Dispose();
+        }
+    }
+}
---
+++ VContainer.Microsoft.DependencyInjectionTests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VContainer.Microsoft.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection.Specification;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VContainer.Microsoft.DependencyInjection.Tests
+{
+    [TestClass()]
+    public class ServiceCollectionExtensionsTests : DependencyInjectionSpecificationTests
+    {
+        protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+        {
+            return serviceCollection.CreateVContainerServiceProvider();
+        }
+    }
+}
